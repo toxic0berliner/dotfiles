db0f830a793d7b4f28b59a945d385eb856bb4c01
etienne <etienne@amato.top>
2024-06-10 01:27:22 +0000

    Adding default package installations

--- install.sh
+++ install.sh
@@ -12,12 +12,12 @@ if [ ! -f $dotfiles/stowaway ]; then
   log debug "Downloading stowaway..."
   download  https://github.com/jamesbehr/stowaway/releases/latest/download/stowaway-linux-amd64.tar.gz $dotfiles/
   download  https://github.com/jamesbehr/stowaway/releases/latest/download/stowaway-linux-amd64.tar.gz.sha256sum $dotfiles/
-  log debug "Verifying checksum..."
+  log debug "  Verifying checksum..."
   d=$(cd $dotfiles; sha256sum -c stowaway-linux-amd64.tar.gz.sha256sum)
   log debug "sha256 check: $d"
   if [ "$(echo $d | grep OK | wc -l)" -le 0 ]; then
-    log debug "Last command: $?"
-    log error "Unable to download stowaway."
+    log debug "    Last command: $?"
+    log error "  Unable to download stowaway."
   else
     log info "Installing stowaway..."
     tar -xzf $dotfiles/stowaway-linux-amd64.tar.gz -C $dotfiles/ >/dev/null 2>&1
@@ -37,15 +37,15 @@ for d in $(find . -type d); do
     ff="${f:2}"
     if [ -f $HOME/$f ]; then
       backupfile="origin.bak/${ff//\//-}.$suffix"
-      log info "backing up $(basename $f) from $HOME to $backupfile"
+      log info "    backing up $(basename $f) from $HOME to $backupfile"
       cp $HOME/$f $dotfiles/$backupfile
       if [ "$?" -ne 0 ]; then
-        log error "Unable to backup $(basename $f) ($f) from $HOME to $dotfiles/$backupfile"
+        log error "        Unable to backup $(basename $f) ($f) from $HOME to $dotfiles/$backupfile"
       else
         rm $HOME/$f
       fi
     else
-      log debug "no prior $ff found in $HOME/${d:2}"
+      log debug "    no prior $ff found in $HOME/${d:2}"
     fi
   done
 done
@@ -53,5 +53,38 @@ done
 log info "Running stowaway..."
 cd $dotfiles
 ./stowaway stow home --target $HOME
+
+# Check if we can install packages
+sudoavailable=false
+if command -v sudo >/dev/null 2>&1; then
+    sudoavailable=true
+fi
+if [ $sudoavailable ]; then 
+  sudo_response=$(SUDO_ASKPASS=/bin/false sudo -A whoami 2>&1 | wc -l)
+  if [ $sudo_response = 2 ]; then
+      can_sudo=1
+  elif [ $sudo_response = 1 ]; then
+      can_sudo=0
+  else
+      log debug "Unexpected sudo response: $sudo_response"
+  fi
+else # sudo is not available
+  can_sudo=0
+fi
+
+id=$(id)
+if [ "$(whoami)" == "root" ] || [ ${id:0:5} == "uid=0" ]; then 
+  is_root=1
+  log debug "You are root 2 !"
+else
+ is_root=false
+fi
+
+if [ $can_sudo ] || [ $is_root ]; then
+  log info "Installing needed packages..."
+  pkginstall tmux git vim tree
+else
+  log warning "Unable to install packages: neither root nor sudoer."
+fi
 
 log info "dotfiles install completed."
